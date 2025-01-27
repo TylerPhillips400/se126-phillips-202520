b772150d69d880c61240dc3437fccd0df4a35b90
TylerPhillips400 <tjphillips1@email.neit.edu>
2025-01-27 09:57:15 -0500

    Finished W3_InClass

--- week3/week3_InClassLab.py
+++ week3/week3_InClassLab.py
@@ -36,59 +36,96 @@ def clear():    #<--FUNCTION HEADER
 
 clear()     #Clear Terminal
 
-total_rec = 0
+#CREATE AN EMPTY LIST for every *potential* field in the file
+#Always base your number of lists on the longest record ie the one with the most field data
+comptype = []       #Computer type -> D or L
+manu = []           #Manufacturer -> DL, GW, or HP
+proc = []           #Processor type
+ram = []            #Total RAM
+hd_1 = []           #Hard drive #1
+
+num_hd = []         #Hard drive #2 * even though not all records have this,  still create an empty list
+
+hd_2 = []           
+os = []             #Operating system
+yr = []             #Machine year
+
+#total_rec = 0
 
 #--connected to file------------------------------------------
 with open("week2/filehandling.csv") as csvfile:
 
     #Reader function to read csvfile
     file = csv.reader(csvfile)
-
-    #Labels for Data
-    print(f"{'TYPE':10}  {'BRAND':10} {'CPU':5}   {'RAM':5}   {'1st Disk':10} {'No HDDS':10}   {'2nd Disk':10} {'OS'}   {'YR'}")
-    print("--------------------------------------------------------------------------------------------------")
     
     for rec in file:
 
         #Counting Variable
-        total_rec += 1
+        #total_rec += 1
         
-        #Converting If Statements
-        type = rec[0]
-        if type == "D":
-            type = "Desktop"
+        if rec[0] == "D":
+            comptype.append("Desktop")
         else:
-            type = "Laptop"
+            comptype.append("Laptop")
 
-        brand = rec[1]
-        if brand == "DL":
-            brand = "Dell"
-        elif brand == "GW":
-            brand = "Gateway"
+        if rec[1] == "DL":
+            manu.append("Dell")
+        elif rec[1] == "GW":
+            manu.append("Gateway")
+        else:
+            manu.append("HP")
         
-        #Record storing to variables
-        cpu = rec[2]
-        ram = rec[3]
-        disk1 = rec[4]
-        numHDDs = rec[5]
+        proc.append(rec[2])
+        ram.append(rec[3])
+        hd_1.append(rec[4])
+        num_hd.append(rec[5])
 
         #Allowing processor to process different amount of fields in each record
-        if numHDDs == "1":
-            disk2 = "-----"
-            os = rec[6]
-            year = rec[7]
+        if rec[5] == "1":
+            hd_2.append("-----")
+            os.append(rec[6])
+            yr.append(rec[7])
         else:
-            disk2 = rec[6]
-            os = rec[7]
-            year = rec[8]
+            hd_2.append(rec[6])
+            os.append(rec[7])
+            yr.append(rec[8])
         
-        print(f"{type:10}  {brand:10} {cpu:5}   {ram:5}   {disk1:10} {numHDDs:10}   {disk2:10} {os}  {year}")
+        #print(f"{type:10}  {brand:10} {cpu:5}   {ram:5}   {disk1:10} {numHDDs:10}   {disk2:10} {os}  {year}")
 
 
-    
-        
-        
+
 #--disconnected from file---------------------------------------
-#Final print statements
+#print(f"{total_rec} Computers processed")
+
+#process data from lists using a FOR loop *after* you are no longer connected to the file
+
+#Labels for Data
+print(f"{'TYPE':10}  {'BRAND':10} {'CPU':5}   {'RAM':5}   {'1st Disk':10} {'No HDDS':10}   {'2nd Disk':10} {'OS'}   {'YR'}")
 print("--------------------------------------------------------------------------------------------------")
-print(f"{total_rec} Computers processed")
+
+#just printing
+for i in range(0, len(comptype)):
+    print(f"{comptype[i]:10}  {manu[i]:10} {proc[i]:5}   {ram[i]:5}   {hd_1[i]:10} {num_hd[i]:10}   {hd_2[i]:10} {os[i]}  {yr[i]}")
+
+print("--------------------------------------------------------------------------------------------------")
+#counting for desktops and laptops that are old
+old_desk = 0        #from 2016 or earlier
+old_lap = 0
+
+for i in range(0, len(yr)):
+
+    if int(yr[i]) <= 16: #Too old
+        print(f"old machine found om index {i}")
+        if comptype[i] == "Desktop":
+            old_desk += 1
+        elif comptype[i] == "Laptop":
+            old_lap += 1
+    
+
+#Final print statements
+print(f"\nTo replace {old_desk} it will cost ${old_desk * 2000:6}")
+print(f"To replace {old_lap} it will cost ${old_lap * 1500:6}")
+
+input("Press ant key to continue... ")
+
+print("\nThank you for using the program. Goodbye!")
