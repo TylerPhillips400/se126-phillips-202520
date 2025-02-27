4a0634612efd6dbc16995043f6ca079b11a47bac
TylerPhillips400 <tjphillips1@email.neit.edu>
2025-02-27 15:01:50 -0500

    project progress 2/27

--- finalProject/Blackjack.py
+++ finalProject/Blackjack.py
@@ -33,35 +33,44 @@ def deal(num):
     cards_dealt = []
     for x in range(num):
         card = deck.pop()
+        card = suit_symbols[card[0]] + card[1]
         cards_dealt.append(card)
     return cards_dealt
 
 def value_Convert(rank):
     value = 0
-    if rank == 'Ace':
+    if rank == 'A':
         value = 11
-    elif rank == 'Jack' or rank == 'Queen' or rank == 'King':
+    elif rank == 'J' or rank == 'Q' or rank == 'K' or rank == '10':
         value = 10
     else:
         value = int(rank)
     return int(value)
 
-
+'''
 def hand_value(card1, card2):
     h_value = 0
     for i in cards_dealt:
         h_value = int(card1 + card2)
     return h_value
-
+'''
 #-Main Code-----------------------------------------------------------------------------------
 
 clear()     #Clear Terminal
 
 #Empty lists
 deck = []
+ranks = []
+suits = []
 cards = []
 player = []
 dealer = []
+
+#Dictionary
+suit_symbols = {"Hearts": "♥", 
+                "Diamonds": "♦", 
+                "Spades": "♠", 
+                "Clubs": "♣"}
 
 #Counting variable
 
@@ -73,28 +82,53 @@ with open("finalProject/cards.csv") as csvfile:
 
     for rec in file:
         deck.append(rec)
+        suits.append(rec[0])
+        ranks.append(rec[1])
 #--disconnected from file---------------------------------------
 
-#Deck Display
+
+#shuffle()
+ans = "y"
 '''
-print(f"{'SUIT':10} {'RANK'}")
-print("---------------------------------------------")
-for i in range(len(deck)):
-    print(f"{deck[i][0]:10} {deck[i][1]}")
-print("---------------------------------------------")
-'''
+ans = input("\nWould you like to play Blackjack? [y/n]: ").lower()
+
+view = input("Would you like to see all cards in the deck? [y/n]: ").lower()
+if view == "y":
+    print(f"{'\nSUIT'} {'RANK'}")
+    print("---------------------------------------------")
+    for j in range(len(suits)):
+        print(f"{suit_symbols[suits[j]]:4} {ranks[j]}")
+    print("---------------------------------------------")
+    input("\nPress ENTER to shuffle the deck...")
+    '''
 shuffle()
 
-cards_dealt = deal(2)
-print(cards_dealt)
+while ans == "y":
+    deal_dealer = []
+    deal_player = []
+    deal_dealer = deal(2)
+    dealer.append(deal_dealer)
+    deal_player = deal(2)
+    player.append(deal_player)
+
+    print(dealer)
+    print(player)
+    ans = "x"
+
+
+
+
 
 print()
-print(cards_dealt[0][1])
-print(cards_dealt[1][1])
 
-card1 = value_Convert(cards_dealt[0][1])
-card2 = value_Convert(cards_dealt[1][1])
+card1 = value_Convert(player[0][0])
+card2 = value_Convert(player[1][0])
+card3 = value_Convert(dealer[0][0])
+card4 = value_Convert(dealer[1][0])
 
 
-
+'''
 print(hand_value(card1, card2))
+
+print(hand_value(card3, card4))
+'''
